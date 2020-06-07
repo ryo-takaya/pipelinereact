eed2af47bf537dbfc2c8dea81385378af9df2918
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-06-08 08:36:58 +0900

    fixed

--- solo_MVP/knexfile.js
+++ solo_MVP/knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
       database: process.env.DATABASE,
       user: process.env.USER,
       password: process.env.PASSWORD,
-      port: process.env.PORT,
+      port: process.env.DBPORT,
     },
     migrations: {
       directory: "./db/migrations",
